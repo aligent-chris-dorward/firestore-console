63c3a6fc8e321af20edc6b9401cd58f4b5987e59
Chris Dorward <chris.dorward@aligent.com.au>
2017-11-01 10:26:28 +1030

    Strip out boilerplate

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { browserHistory, Router } from 'react-router'
-import { Provider } from 'react-redux'
-import { Notifications } from 'modules/notification'
+/**
+ * Firestore Console
+ *
+ * @category    listingslab
+ * @package     firestore-console
+ * @repo        https://github.com/listingslab/firestore-console
+ * @author      Chris Dorward <listingslab@gmail.com>
+ * @license     OSL-3.0
+ *
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { browserHistory, Router } from 'react-router';
+import { Provider } from 'react-redux';
+import { Notifications } from 'modules/notification';
 
 // Themeing/Styling
-import Theme from 'theme'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import Theme from 'theme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 // Tap Plugin
-import injectTapEventPlugin from 'react-tap-event-plugin'
+import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin()
 
 export default class AppContainer extends Component {
--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,15 @@
+/**
+ * Firestore Console
+ *
+ * @category    listingslab
+ * @package     firestore-console
+ * @repo        https://github.com/listingslab/firestore-console
+ * @author      Chris Dorward <listingslab@gmail.com>
+ * @license     OSL-3.0
+ *
+ */
+
+
 import {
   blueGrey100,
   blueGrey500,
